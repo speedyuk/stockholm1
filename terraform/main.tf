f8f403d981ea796bf9d4c120c7d3a648df083276
Cliff Ainsworth <36737624+speedyuk@users.noreply.github.com>
2022-04-07 09:40:30 +0200

    updates

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,6 @@
 provider "kubernetes" {
   config_path    = "~/.kube/config"
-  config_context = "aks-northeurope"
+  config_context = "cliff-naplab-aks-cluster"
 }
 
 resource "kubernetes_deployment" "nginx-nap" {
@@ -33,7 +33,7 @@ resource "kubernetes_deployment" "nginx-nap" {
 
       spec {
         container {
-          image = "registryemeasa.azurecr.io/nginx/nap:v1.0"
+          image = "regcliff.azurecr.io/nginx/nap:v1.0"
           name  = "nginx-nap"
           image_pull_policy = "Always"
 
